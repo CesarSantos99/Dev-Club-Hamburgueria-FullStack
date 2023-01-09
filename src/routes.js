7790728be0927d6009afbd4644270523b6eaf9f1
Cesar Santos <cesarsantossouza99@gmail.com>
2023-01-09 18:47:12 -0300

    em andamento

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+
+const routes = new Router()
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Hello Word' })
+})
+
+export default routes
