9a0216db9b58f0f0750de5ef3783282430087d1c
Cesar Santos <cesarsantossouza99@gmail.com>
2023-01-10 20:29:42 -0300

    add usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,17 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+import User from './app/model/User'
 
 const routes = new Router()
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello Word' })
+
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Cesar',
+    email: 'cesarsantos@email.com',
+    password_hash: '56789gb',
+  })
+  return response.json(user)
 })
 
 export default routes
