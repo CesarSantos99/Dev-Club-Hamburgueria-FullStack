966e8dd87c05fd5beba8d6a962ee09f063f2faab
Cesar Santos <cesarsantossouza99@gmail.com>
2023-01-13 18:39:27 -0300

    criptografano a senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -9,17 +9,33 @@ class UserController {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string().email().required(),
-      password_hash: Yup.string().required().min(6),
+      password: Yup.string().required().min(6),
       admin: Yup.boolean(),
     })
 
-    const { name, email, password_hash, admin } = request.body
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ erro: err.errors })
+    }
+
+    const { name, email, password, admin } = request.body
+
+    const userExists = await User.findOne({
+      where: { email },
+    })
+
+    if (userExists) {
+      return response.status(400).json({ error: 'User already exists' })
+    }
+
+    console.log(userExists)
 
     const user = await User.create({
       id: v4(),
       name,
       email,
-      password_hash,
+      password,
       admin,
     })
 
