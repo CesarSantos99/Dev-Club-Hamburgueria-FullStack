e0d95638d36a5f3d2dfd82c7ecfd6e198c1c441e
Cesar Santos <cesarsantossouza99@gmail.com>
2023-01-18 20:02:39 -0300

    uploads de imagens

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import ProdutController from './app/controllers/ProdutController'
 import SessionController from './app/controllers/SessionController'
 
 import UserController from './app/controllers/UserController'
@@ -8,5 +9,7 @@ const routes = new Router()
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
+
+routes.post('/products', ProdutController.store)
 
 export default routes
